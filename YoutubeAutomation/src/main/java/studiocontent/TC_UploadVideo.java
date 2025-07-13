02123f03dc7a84e3f598a92597aabd04ec62d2b0
bones <bones@Aorus-Pro-X>
2025-07-13 00:37:12 -0400

    TC_UploadVideo

--- YoutubeAutomation/src/main/java/studiocontent/TC_UploadVideo.java
+++ YoutubeAutomation/src/main/java/studiocontent/TC_UploadVideo.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -87,7 +88,9 @@ public class TC_UploadVideo {
 		wait.until(d -> driver.findElement(By.cssSelector("#close-button > ytcp-button-shape > button")).isDisplayed());
 		WebElement closeButton = driver.findElement(By.cssSelector("#close-button > ytcp-button-shape > button"));
 		closeButton.click();
-				
+		
+		Thread.sleep(1200);//waiting because the video doesn't show up in the list when moving too quickly
+		
 		WebElement contentButton = driver.findElement(By.cssSelector("#menu-paper-icon-item-1 > div.nav-item-text.style-scope.ytcp-navigation-drawer"));
 		contentButton.click();
 		
@@ -95,10 +98,14 @@ public class TC_UploadVideo {
 		WebElement shortsTab = driver.findElement(By.cssSelector("#video-list-shorts-tab > div > ytcp-ve > span"));
 		shortsTab.click();
 		
+		Thread.sleep(1000);//1 sec wait for tab transition
+		
 		ExtentTest webmStep = test.createNode("WebM file should be uploaded");
 		boolean webmPass = false;
+		
 		try {
 			WebElement videoTitle = driver.findElement(By.id("video-title"));
+			
 			if(videoTitle.getText().contains("catvideo")) {
 				webmStep.pass("The WebM video was uploaded", MediaEntityBuilder.createScreenCaptureFromBase64String(scrShot.getScreenshotAs(OutputType.BASE64)).build());
 				webmPass = true;
@@ -106,100 +113,125 @@ public class TC_UploadVideo {
 			else {
 				webmStep.fail("Unable to find the WebM video upload title", MediaEntityBuilder.createScreenCaptureFromBase64String(scrShot.getScreenshotAs(OutputType.BASE64)).build());//failed the step and taking a screenshot
 			}
+			
+			//Deleting the upload so that future tests are not affected
+			
+			//Setting up a hover action to be able to click on the kebab button
+			Actions action = new Actions(driver);
+			action.moveToElement(videoTitle).perform();
+
+			WebElement kebabButton = driver.findElement(By.cssSelector("#hover-items > ytcp-icon-button > tp-yt-iron-icon"));
+			action.moveToElement(kebabButton);
+			action.click().perform();
+			
+			WebElement deleteButton = driver.findElement(By.cssSelector("#text-item-5 > ytcp-ve > tp-yt-paper-item-body > div > div > div > yt-formatted-string"));
+			deleteButton.click();
+			
+			wait.until(d -> driver.findElement(By.id("confirm-checkbox")).isDisplayed());
+			WebElement deleteCheckbox = driver.findElement(By.id("confirm-checkbox"));
+			deleteCheckbox.click();
+			
+			WebElement finalDeleteButton = driver.findElement(By.id("confirm-button"));
+			finalDeleteButton.click();
+
 		}
 		catch (NoSuchElementException n){
 			webmStep.fail("Unable to find the WebM video upload element", MediaEntityBuilder.createScreenCaptureFromBase64String(scrShot.getScreenshotAs(OutputType.BASE64)).build());//failed the step and taking a screenshot
 			n.printStackTrace();
 		}
 		
-		//Deleting the upload so that future tests are not affected
-		WebElement kebabButton = driver.findElement(By.cssSelector("#hover-items > ytcp-icon-button > tp-yt-iron-icon"));
-		kebabButton.click();
-		
-		WebElement deleteButton = driver.findElement(By.linkText("Delete forever"));
-		deleteButton.click();
-		
-		WebElement deleteCheckbox = driver.findElement(By.id("checkbox-container"));
-		deleteCheckbox.click();
-		
-		WebElement finalDeleteButton = driver.findElement(By.cssSelector("#confirm-button > ytcp-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill"));
-		finalDeleteButton.click();
-		
+
 		
 		//Doing the same process for uploading an MP4 file and filling the required fields
 		//
-		wait.until(d -> driver.findElement(By.cssSelector("#upload-icon > tp-yt-iron-icon")));
-		//WebElement uploadButton = driver.findElement(By.cssSelector("#upload-icon > tp-yt-iron-icon"));
-		uploadButton.click();
+		wait.until(d -> driver.findElement(By.id("create-icon")));
+		WebElement createButton = driver.findElement(By.id("create-icon"));
+		createButton.click();
 		
-		//WebElement selectFileButton = driver.findElement(By.name("Filedata"));
+		wait.until(d -> driver.findElement(By.cssSelector("#text-item-0 > ytcp-ve > tp-yt-paper-item-body > div > div > div > yt-formatted-string")));
+		WebElement uploadVideosButton = driver.findElement(By.cssSelector("#text-item-0 > ytcp-ve > tp-yt-paper-item-body > div > div > div"));
+		uploadVideosButton.click();
+		
+		WebElement selectFileButton2 = driver.findElement(By.name("Filedata"));
 		File video2 = new File("target/catvideo.mp4");//instead of manually entering the absolute path, having the file saved to the variable and calling getAbsolutePath() so anyone that downloads the repository can run it without changing the filepath
-		selectFileButton.sendKeys(video1.getAbsolutePath());
+		selectFileButton2.sendKeys(video1.getAbsolutePath());
 		
 		wait.until(d -> driver.findElement(By.name("VIDEO_MADE_FOR_KIDS_NOT_MFK")).isDisplayed());
-		//WebElement kidsRadioButton = driver.findElement(By.name("VIDEO_MADE_FOR_KIDS_NOT_MFK"));//clicking on mandatory radio button option
-		kidsRadioButton.click();
+		WebElement kidsRadioButton2 = driver.findElement(By.name("VIDEO_MADE_FOR_KIDS_NOT_MFK"));//clicking on mandatory radio button option
+		kidsRadioButton2.click();
 		
-		//WebElement nextButton = driver.findElement(By.cssSelector("#next-button > ytcp-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill"));
-		nextButton.click();
-		wait.until(d -> nextButton.isDisplayed());
-		nextButton.click();
-		wait.until(d -> nextButton.isDisplayed());
-		nextButton.click();
+		WebElement nextButton2 = driver.findElement(By.cssSelector("#next-button > ytcp-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill"));
+		nextButton2.click();
+		wait.until(d -> nextButton2.isDisplayed());
+		nextButton2.click();
+		wait.until(d -> nextButton2.isDisplayed());
+		nextButton2.click();
 		
-		//WebElement privateRadioButton = driver.findElement(By.id("private-radio-button"));
-		privateRadioButton.click();
+		WebElement privateRadioButton2 = driver.findElement(By.id("private-radio-button"));
+		privateRadioButton2.click();
 		
-		//WebElement saveButton = driver.findElement(By.id("done-button"));
-		saveButton.click();
+		WebElement saveButton2 = driver.findElement(By.id("done-button"));
+		saveButton2.click();
 		
 		while(driver.getPageSource().contains("Disabled while we prepare your video")){//loop because the Save button is disabled during processing
 			Thread.sleep(500);
-			saveButton.click();
+			saveButton2.click();
 			}
 		
 		//Checking for the MP4 video file
 		//
 		wait.until(d -> driver.findElement(By.cssSelector("#close-button > ytcp-button-shape > button")).isDisplayed());
-		//WebElement closeButton = driver.findElement(By.cssSelector("#close-button > ytcp-button-shape > button"));
-		closeButton.click();
+		WebElement closeButton2 = driver.findElement(By.cssSelector("#close-button > ytcp-button-shape > button"));
+		closeButton2.click();
 				
-		//WebElement contentButton = driver.findElement(By.cssSelector("#menu-paper-icon-item-1 > div.nav-item-text.style-scope.ytcp-navigation-drawer"));
-		contentButton.click();
+		WebElement contentButton2 = driver.findElement(By.cssSelector("#menu-paper-icon-item-1 > div.nav-item-text.style-scope.ytcp-navigation-drawer"));
+		contentButton2.click();
 		
 		wait.until(d -> driver.findElement(By.cssSelector("#video-list-shorts-tab > div > ytcp-ve > span")).isDisplayed());
-		//WebElement shortsTab = driver.findElement(By.cssSelector("#video-list-shorts-tab > div > ytcp-ve > span"));
-		shortsTab.click();
+		WebElement shortsTab2 = driver.findElement(By.cssSelector("#video-list-shorts-tab > div > ytcp-ve > span"));
+		shortsTab2.click();
 		
 		ExtentTest mp4Step = test.createNode("MP4 file should be uploaded");
 		boolean mp4Pass = false;
+		
+		Thread.sleep(1000);//1 sec wait for tab transition
+		
 		try {
-			WebElement videoTitle = driver.findElement(By.id("video-title"));
-			if(videoTitle.getText().contains("catvideo")) {
+			WebElement videoTitle2 = driver.findElement(By.id("video-title"));
+			
+			if(videoTitle2.getText().contains("catvideo")) {
 				mp4Step.pass("The MP4 video was uploaded", MediaEntityBuilder.createScreenCaptureFromBase64String(scrShot.getScreenshotAs(OutputType.BASE64)).build());
 				mp4Pass = true;
 			}
 			else {
 				mp4Step.fail("Unable to find the MP4 video upload title", MediaEntityBuilder.createScreenCaptureFromBase64String(scrShot.getScreenshotAs(OutputType.BASE64)).build());//failed the step and taking a screenshot
 			}
+			
+			//Deleting the upload so that future tests are not affected
+			
+			//Setting up a hover action to be able to click on the kebab button
+			Actions action = new Actions(driver);
+			action.moveToElement(videoTitle2).perform();
+
+			WebElement kebabButton2 = driver.findElement(By.cssSelector("#hover-items > ytcp-icon-button > tp-yt-iron-icon"));
+			action.moveToElement(kebabButton2);
+			action.click().perform();
+			
+			WebElement deleteButton2 = driver.findElement(By.cssSelector("#text-item-5 > ytcp-ve > tp-yt-paper-item-body > div > div > div > yt-formatted-string"));
+			deleteButton2.click();
+			
+			wait.until(d -> driver.findElement(By.id("confirm-checkbox")).isDisplayed());
+			WebElement deleteCheckbox2 = driver.findElement(By.id("confirm-checkbox"));
+			deleteCheckbox2.click();
+			
+			WebElement finalDeleteButton2 = driver.findElement(By.id("confirm-button"));
+			finalDeleteButton2.click();
 		}
 		catch (NoSuchElementException n){
 			mp4Step.fail("Unable to find the MP4 video upload element", MediaEntityBuilder.createScreenCaptureFromBase64String(scrShot.getScreenshotAs(OutputType.BASE64)).build());//failed the step and taking a screenshot
 			n.printStackTrace();
 		}
 		
-		//Deleting the upload so that future tests are not affected
-		//WebElement kebabButton = driver.findElement(By.cssSelector("#hover-items > ytcp-icon-button > tp-yt-iron-icon"));
-		kebabButton.click();
-		
-		//WebElement deleteButton = driver.findElement(By.linkText("Delete forever"));
-		deleteButton.click();
-		
-		//WebElement deleteCheckbox = driver.findElement(By.id("checkbox-container"));
-		deleteCheckbox.click();
-		
-		//WebElement finalDeleteButton = driver.findElement(By.cssSelector("#confirm-button > ytcp-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill"));
-		finalDeleteButton.click();
 		
 		
 		//If both steps passed then the test case passes
